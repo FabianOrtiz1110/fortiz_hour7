a4fd4d16c70beba82146df222e4d63b8f3862abd
FabianOrtiz1110 <112835551+FabianOrtiz1110@users.noreply.github.com>
2023-03-09 11:19:27 -0800

    new
    new

--- Assets/MONTHS.cs
+++ Assets/MONTHS.cs
@@ -7,14 +7,32 @@ public class MONTHS : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 1; i <= 12; i++)
+        //Prints out Months in number format with birth month printing a line.
+        for (int i = 1; i <= 3; i++)
+        {
+            Debug.Log(i);
+        }
+        Debug.Log("It's my birthday month");
+        for (int i = 5; i <= 12; i++)
+        {
+            Debug.Log(i);
+        }
+
+        //Prints number of days in april with birthday printing a different line.
+        for (int i = 1; i <= 13; i++)
+        {
+            Debug.Log(i);
+        }
+
+        Debug.Log("It's my birthday");
+
+        for (int i = 15; i <= 30; i++)
         {
             Debug.Log(i);
         }
     }
-
-    // Update is called once per frame
-    void Update()
+        // Update is called once per frame
+        void Update()
     {
         
     }
