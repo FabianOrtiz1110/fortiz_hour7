700286c602425b5f3363f2c16683474c8c894963
FabianOrtiz1110 <112835551+FabianOrtiz1110@users.noreply.github.com>
2023-03-09 08:36:19 -0800

    its new
    new

---
+++ Assets/MONTHS.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MONTHS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i =1; i<= 12; i+=1)
+            Debug.Log(i);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
